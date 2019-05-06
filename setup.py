9a8fb77ca765d9ae085f452fe3165f4c651612bd
Vignesh <user@prodapt.com>
2019-05-06 21:00:54 +0530

    first commit

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="example_package",
     version="0.0.1",
     author="Example Author",
-    author_email="author@example.com",
+    author_email="vigneshkumar.g@prodapt.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/gvickyviki1/m_frame",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
